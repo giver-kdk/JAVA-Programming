f88d669be51ffc730113ea714f0624d9bb718c86
Revig <giverbro13@gmail.com>
2024-09-25 07:46:11 +0545

    UDP Update

--- tcp_udp/src/tcp_udp/udp_server.java
+++ tcp_udp/src/tcp_udp/udp_server.java
@@ -16,13 +16,13 @@ public class udp_server {
         try{
             DatagramSocket socket = new DatagramSocket(9999);
             while(true){
-            byte[] receiveData = new byte[100];    
-            
-            DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
-            socket.receive(receivePacket);
-            
-            String receiveMessage = new String(receivePacket.getData());
-            System.out.println("Client Message: " + receiveMessage); 
+                byte[] byteData = new byte[100];    
+
+                DatagramPacket packet = new DatagramPacket(byteData, byteData.length);
+                socket.receive(packet);
+
+                String msg = new String(packet.getData());
+                System.out.println("Client Message: " + msg); 
             }
         }
         catch(Exception e){
