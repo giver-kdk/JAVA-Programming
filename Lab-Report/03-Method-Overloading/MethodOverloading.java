5fb4e51251db560303843b3ae3cc3154e2c94f87
giver-kdk <giverbro13@gmail.com>
2025-01-26 19:33:30 +0545

    FIle Relocations

---
+++ Lab-Report/03-Method-Overloading/MethodOverloading.java
@@ -0,0 +1,24 @@
+class MethodOverloading {
+
+    // Method to calculate area of a rectangle (length * width)
+    public double fingArea(double length, double width) {
+        return length * width;
+    }
+
+    // Overloaded method to calculate area of a circle (π * radius^2)
+    public double fingArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static void main(String[] args) {
+        MethodOverloading calculator = new MethodOverloading();
+
+        // Calculate and print area of rectangle
+        double rectangleArea = calculator.fingArea(5.0, 3.0);
+        System.out.println("Area of Rectangle: " + rectangleArea);
+
+        // Calculate and print area of circle
+        double circleArea = calculator.fingArea(4.0);
+        System.out.println("Area of Circle: " + circleArea);
+    }
+}
